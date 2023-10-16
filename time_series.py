eacf104e25029e211efba143695f84aa2f428fd1
Juanita Palomar <36558688+jjpal@users.noreply.github.com>
2023-10-16 11:51:57 -0500

    Update time_series.py

--- time_series.py
+++ time_series.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(layout="wide")
+
 st.title('Time Series Analysis')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = (' ')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL)
